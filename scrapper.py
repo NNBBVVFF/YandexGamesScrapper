0b66d4705f39c60cc0da37db8efaa448f6f2cef1
NNBBVVFF <sataev.telecom@gmail.com>
2022-08-01 11:30:10 +0300

    Подсоединение всех папок к проекту

--- scrapper.py
+++ scrapper.py
@@ -1,17 +1,44 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#import csv
+from bs4 import BeautifulSoup
+import csv
 import time
 import random
 import requests
 
 #страничка яндекс-игорей
 root_url = "https://yandex.ru/games/category/new"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+    "X-Amzn-Trace-Id": "Root=1-62b76434-418767bb1710570a46186842"}
+
 game_url_list = []
 
 def main():
-    #get_urls()
-    game_info("https://yandex.ru/games/?app=192879")
+    get_urls()
+    for card_url in game_url_list:
+        response = requests.get(card_url, headers=headers)
+        soup = BeautifulSoup(response.text, "lxml")
+        name_of_game = soup.find('h1', class_="game-page__title").text
+        count_of_download = soup.find("span", class_="game-number__number-text").text
+        # изначально count_of_download содержит неразрывные пробелы, применяем функцию OnlyDigits, перезаписывая все символы, кроме цифр
+        count_of_download = OnlyDigits(count_of_download)
+        rate_of_game = soup.find("span", class_="popularity-badge__rating")
+        if rate_of_game is None:
+            print("No rate, young game, beatch" + str(rate_of_game))
+            rate_of_game = 0
+
+        else:
+            rate_of_game = rate_of_game.text
+            rate_of_game = float(rate_of_game.replace(',', '.'))
+        # print(name_of_game, count_of_download, rate_of_game, sep='\n', end="\n\n")
+
+        with open('scrap_result.csv', mode='a', encoding="utf-8") as file:
+            writer = csv.writer(file)
+            print(name_of_game, count_of_download, rate_of_game)
+            outlist = [name_of_game, count_of_download, rate_of_game]
+            writer.writerow(outlist)
 
 def OnlyDigits(text):
     return "".join([i for i in text if i.isdigit()])
@@ -39,31 +66,6 @@ def get_urls():
                 game_url = game.get_attribute("href")
                 print(game_url)
                 game_url_list.append(game_url)
-
-
-def game_info(link):
-    browser2 = webdriver.Chrome()
-    # browser2.implicitly_wait(10)
-    browser2.maximize_window()
-    browser2.get(link)
-    PageSource = browser2.page_source
-    print(PageSource)
-    # try:
-    #     rate_of_game = browser2.find_element(By.CLASS_NAME, "popularity-badge__rating").text
-    # except Exception:
-    #     rate_of_game = "Игра слишком свежая, оценок ещё мало"
-    #     print(rate_of_game)
-    # else:
-    #     print(rate_of_game)
-    # finally:
-    #     name_of_game = browser2.find_element(By.CSS_SELECTOR, ".game-page__title").text
-    #     print(name_of_game)
-    #     count_of_download = OnlyDigits(browser2.find_element(By.CSS_SELECTOR, ".game-number__number-text").text)
-    #     print(count_of_download)
-
-
-
-
 
 if __name__ == "__main__":
     main()
